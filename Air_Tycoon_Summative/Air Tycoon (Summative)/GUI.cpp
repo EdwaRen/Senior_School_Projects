185f449d6809308fed06dac18636a1e561bb1c5a
Edward Ren <eddie.ren.2013@gmail.com>
2017-04-24 14:37:22 -0400

    Created a CLI GUI, updated readme and UML Diagram

---
+++ Air_Tycoon_Summative/Air Tycoon (Summative)/GUI.cpp
@@ -0,0 +1,9 @@
+//
+//  GUI.cpp
+//  Air Tycoon (Summative)
+//
+//  Created by - on 2017/04/24.
+//  Copyright © 2017 Eddie of the House of Ren. All rights reserved.
+//
+
+#include "GUI.hpp"
---
+++ Air_Tycoon_Summative/Air Tycoon (Summative)/GUI.hpp
@@ -0,0 +1,14 @@
+//
+//  GUI.hpp
+//  Air Tycoon (Summative)
+//
+//  Created by - on 2017/04/24.
+//  Copyright © 2017 Eddie of the House of Ren. All rights reserved.
+//
+
+#ifndef GUI_hpp
+#define GUI_hpp
+
+#include <stdio.h>
+
+#endif /* GUI_hpp */
